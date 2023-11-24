ebcc7154b3423fb5c45aaaab0fe027ce15758181
Kövesdi Tamás <34796547+Kovesditomi@users.noreply.github.com>
2023-11-24 12:34:51 +0100

    Add files via upload

---
+++ szamitogepek/szamitogepek/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace szamitogepek
+{
+    internal class Program
+    {
+        static List<Szamitogep> Szamitogeps(List<Szamitogep> a);
+        
+
+        static void Main(string[] args)
+        {
+            var szamitogepek=new List<Szamitogep>();
+            using var sr = new StreamReader(@"..\..\..\src\szamitogepek.txt");
+            { 
+            
+            sr.ReadLine();
+                while (sr.EndOfStream)
+                {
+                    szamitogepek.Add(new Szamitogep(sr.ReadLine()));
+                }
+           
+            }
+            foreach (var item in szamitogepek)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("7. feladat");
+            foreach (var item in Szamitogeps(szamitogepek));
+            {
+                if (item = null)
+                {
+
+                }
+            
+            }
+        }
+    }
+}
